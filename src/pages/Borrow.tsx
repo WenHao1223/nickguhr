39fe6283f52bdd45ce0ee3bc36fc540efafbb7a5
Lim Wen Hao <wenhaojshs@gmail.com>
2024-08-17 18:36:14 +0800

    Fix structure

--- src/pages/Borrow.tsx
+++ src/pages/Borrow.tsx
@@ -1,7 +1,22 @@
+import TokenBox from "@/components/borrow/TokenBox";
+
 function Borrow () {
   return (
     <>
-      <h1>This is a Borrow page.</h1>
+      <h1 className="font-semibold text-3xl">Borrow</h1>
+
+      <div className="flex">
+        <div className="w-1/2 p-4">
+          {/* Left section content */}
+          {/* <p>This is the left section.</p> */}
+          <TokenBox cardTitle="You collateralize"/>
+        </div>
+        <div className="w-1/2 p-4">
+          {/* Right section content */}
+          {/* <p>This is the right section.</p> */}
+          <TokenBox cardTitle="You borrow"/>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/Lend.tsx
+++ src/pages/Lend.tsx
@@ -1,22 +1,11 @@
-import TokenBox from "@/components/lend/TokenBox";
+import TokenBox from "@/components/borrow/TokenBox";
 
 function Lend () {
   return (
     <>
       <h1 className="font-semibold text-3xl">Lend</h1>
 
-      <div className="flex">
-        <div className="w-1/2 p-4">
-          {/* Left section content */}
-          {/* <p>This is the left section.</p> */}
-          <TokenBox cardTitle="You collateralize"/>
-        </div>
-        <div className="w-1/2 p-4">
-          {/* Right section content */}
-          {/* <p>This is the right section.</p> */}
-          <TokenBox cardTitle="You borrow"/>
-        </div>
-      </div>
+      
     </>
   );
 }
