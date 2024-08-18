36d7b768b1a9dd4e69fb223aa8f5488d4df73bc0
Lim Wen Hao <wenhaojshs@gmail.com>
2024-08-18 11:09:01 +0800

    Initial setup of dashboard

--- src/components/dashboard/DashboardCardLayout.tsx
+++ src/components/dashboard/DashboardCardLayout.tsx
@@ -1,12 +1,12 @@
 function DashboardCardLayout({children, img, name}: {children: React.ReactNode, img: string, name: string}
 ) {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl text-left m-4">
+    <div className="card lg:card-side bg-base-100 shadow-xl text-left m-4 w-full">
       <figure className="items-center align-center">
         <img
           src={img}
           alt={name}
-          className="h-40 aspect-w-16 aspect-h-9"/>
+          className="h-20 aspect-w-16 aspect-h-9"/>
       </figure>
       <div className="card-body w-3/5">
         {children}
--- src/components/dashboard/MainDashboard.tsx
+++ src/components/dashboard/MainDashboard.tsx
@@ -1,4 +1,4 @@
-import DashboardCard from "@/components/dashboard/DashboardCardLayout";
+import DashboardCardLayout from "@/components/dashboard/DashboardCardLayout";
 
 function MainDashboard () {
   return (
@@ -6,18 +6,46 @@ function MainDashboard () {
       <div className="w-1/2 p-4">
         {/* Assets overview */}
         <h3 className="font-bold text-lg">Assets Overview</h3>
-        {/* <DashboardCard img="https://cdn-icons-png.flaticon.com/512/4002/4002090.png" name="asset">
-          <h3 className="font-bold text-lg">Total Assets</h3>
+        <DashboardCardLayout img="https://cdn-icons-png.flaticon.com/512/4002/4002090.png" name="asset">
+          <h5 className="font-bold text-sm">Account Value</h5>
           <p>0.00</p>
-        </DashboardCard>
-        <DashboardCard>
-          <h3 className="font-bold text-lg">Total Liabilities</h3>
+          <h5 className="font-bold text-sm">Available Balance</h5>
           <p>0.00</p>
-        </DashboardCard> */}
+        </DashboardCardLayout>
+        <DashboardCardLayout img="https://cdn-icons-png.flaticon.com/512/4002/4002090.png" name="rent">
+          <div className="flex">
+            <div className="w-1/2">
+              <h5 className="font-bold text-sm">Total Rent Earned</h5>
+              <p>$ 0.00</p>
+              <h5 className="font-bold text-sm">Rent Balance</h5>
+              <p>$ 0.00</p>
+            </div>
+            <div className="w-1/2">
+              <h5 className="font-bold text-sm">Properties Owned</h5>
+              <p>$ 0.00</p>
+              <h5 className="font-bold text-sm">Est. Property Value</h5>
+              <p>$ 0.00</p>
+            </div>
+          </div>
+        </DashboardCardLayout>
       </div>
       <div className="w-1/2 p-4">
         {/* My Wallet */}
         <h3 className="font-bold text-lg">My Wallet</h3>
+        <DashboardCardLayout img="https://cdn-icons-png.flaticon.com/512/4002/4002090.png" name="rent">
+          <div className="flex">
+            <div className="w-1/2">
+              <h5 className="font-bold text-sm">Wallet Value</h5>
+              <p>$0.00</p>
+              <h5 className="font-bold text-sm">Crypto Value</h5>
+              <p>$0.00</p>
+            </div>
+            <div className="w-1/2">
+              <h5 className="font-bold text-sm">Property Value</h5>
+              <p>$0.00</p>
+            </div>
+          </div>
+        </DashboardCardLayout>
       </div>
     </div>
   );
