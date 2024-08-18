449fd25147c6798011cc5a76e5e6f6bd1306ffe5
Lim Wen Hao <wenhaojshs@gmail.com>
2024-08-18 09:49:02 +0800

    Add create pool button

--- src/pages/Lend.tsx
+++ src/pages/Lend.tsx
@@ -1,7 +1,31 @@
 import InputSelectToken from "@/components/input/InputSelectToken";
 import InputFixToken from "@/components/input/InputFixToken";
 
+import Swal from "sweetalert2";
+
 function Lend () {
+  const createPool = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, create pool!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Pool created!",
+          text: "You can now lend your assets to borrowers.",
+          icon: "success"
+        });
+
+        window.location.reload();
+      }
+    });
+  }
+
   return (
     <div >
       <h1 className="font-semibold text-3xl">Create Pool</h1>
@@ -37,6 +61,11 @@ function Lend () {
         <div className="flex justify-between items-center">
           <InputFixToken id="lendInterest" defaultVal={5} fixVal="%"/>
         </div>
+      </div>
+
+      {/* Create Pool button */}
+      <div className="mb-4 text-left rounded-md p-4 w-full border border-zinc-800 bg-zinc-800">
+        <button className="btn bg-primary text-zinc-200 w-full" onClick={createPool}>Create Pool</button>
       </div>
     </div>
   );
